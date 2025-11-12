64a42bc1127d5ec01bae5c2aa792df5acce80cf3
Antonín Vídeňský <videnskya24@spstrutnov.cz>
2025-11-12 11:11:09 +0100

    snak
    idk základy +- funkční hra

--- snake/snake/Game1.cs
+++ snake/snake/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
 
 namespace snake
 {
@@ -9,44 +11,179 @@ namespace snake
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        private Texture2D _snakeTexture;
+        private Texture2D _foodTexture;
+
+        private List<Point> _snake;
+        private Point _food;
+        private Point _direction;
+        private double _timer;
+        private double _moveInterval = 0.15; // seconds between moves
+
+        private int _cellSize = 20;
+        private int _gridWidth = 40;
+        private int _gridHeight = 30;
+
+        private bool _gameOver;
+        private Random _random = new Random();
+
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
+
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 600;
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
+            ResetGame();
             base.Initialize();
+        }
+
+        private void ResetGame()
+        {
+            _snake = new List<Point>
+            {
+                new Point(_gridWidth / 2, _gridHeight / 2)
+            };
+
+            _direction = new Point(1, 0);
+            SpawnFood();
+            _gameOver = false;
+            _timer = 0;
+        }
+
+        private void SpawnFood()
+        {
+            _food = new Point(_random.Next(0, _gridWidth), _random.Next(0, _gridHeight));
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            _snakeTexture = new Texture2D(GraphicsDevice, 1, 1);
+            _snakeTexture.SetData(new[] { Color.LimeGreen });
+
+            _foodTexture = new Texture2D(GraphicsDevice, 1, 1);
+            _foodTexture.SetData(new[] { Color.Red });
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            var keyboard = Keyboard.GetState();
+
+            if (keyboard.IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            if (_gameOver)
+            {
+                if (keyboard.IsKeyDown(Keys.Enter))
+                    ResetGame();
+                base.Update(gameTime);
+                return;
+            }
+
+            // Handle direction input
+            if (keyboard.IsKeyDown(Keys.W) && _direction.Y == 0)
+                _direction = new Point(0, -1);
+            else if (keyboard.IsKeyDown(Keys.S) && _direction.Y == 0)
+                _direction = new Point(0, 1);
+            else if (keyboard.IsKeyDown(Keys.A) && _direction.X == 0)
+                _direction = new Point(-1, 0);
+            else if (keyboard.IsKeyDown(Keys.D) && _direction.X == 0)
+                _direction = new Point(1, 0);
+
+            _timer += gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (_timer >= _moveInterval)
+            {
+                _timer = 0;
+
+                Point newHead = new Point(_snake[0].X + _direction.X, _snake[0].Y + _direction.Y);
+
+                // Wall collision
+                if (newHead.X < 0 || newHead.X >= _gridWidth || newHead.Y < 0 || newHead.Y >= _gridHeight)
+                {
+                    _gameOver = true;
+                    return;
+                }
+
+                // Self collision
+                foreach (var part in _snake)
+                {
+                    if (part == newHead)
+                    {
+                        _gameOver = true;
+                        return;
+                    }
+                }
+
+                _snake.Insert(0, newHead);
+
+                if (newHead == _food)
+                {
+                    SpawnFood();
+                }
+                else
+                {
+                    _snake.RemoveAt(_snake.Count - 1);
+                }
+            }
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            // Draw food
+            _spriteBatch.Draw(_foodTexture, new Rectangle(_food.X * _cellSize, _food.Y * _cellSize, _cellSize, _cellSize), Color.Red);
+
+            // Draw snake
+            foreach (var part in _snake)
+            {
+                _spriteBatch.Draw(_snakeTexture, new Rectangle(part.X * _cellSize, part.Y * _cellSize, _cellSize, _cellSize), Color.LimeGreen);
+            }
+
+            // Draw game over text
+            if (_gameOver)
+            {
+                string msg = "Game Over! Press Enter to restart";
+                var font = new SpriteFontStub(GraphicsDevice);
+                font.DrawCenteredText(_spriteBatch, msg, new Vector2(400, 300), Color.White);
+            }
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
+        }
+
+        // simple text helper class
+        private class SpriteFontStub
+        {
+            private Texture2D _pixel;
+            private GraphicsDevice _device;
+
+            public SpriteFontStub(GraphicsDevice device)
+            {
+                _device = device;
+                _pixel = new Texture2D(device, 1, 1);
+                _pixel.SetData(new[] { Color.White });
+            }
+
+            public void DrawCenteredText(SpriteBatch spriteBatch, string text, Vector2 position, Color color)
+            {
+                // This is a placeholder – replace with real SpriteFont if you want prettier text
+                var size = text.Length * 8;
+                spriteBatch.Draw(_pixel, new Rectangle((int)position.X - size / 2, (int)position.Y - 5, size, 10), color);
+            }
         }
     }
 }
